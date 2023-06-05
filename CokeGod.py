6485a7e19581591780994b9386ee3212ae5b1c8d
HeapHieps <117422956+HeapHieps@users.noreply.github.com>
2023-06-05 01:37:25 -0700

    Update CokeGod.py

--- CokeGod.py
+++ CokeGod.py
@@ -7,12 +7,12 @@ client = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
 @client.event
 async def on_ready():
-    print("Logged in as {0.user}".format(client))
+    	print("Logged in as {0.user}".format(client))
 
 
 @client.command()
 async def ping(ctx, arg):
-    await ctx.send(arg)
+    	await ctx.send(arg)
 
 @client.command()
 async def meoow(ctx, member: discord.Member):
@@ -25,31 +25,34 @@ async def meoow(ctx, member: discord.Member):
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
-        return
+	if message.author == client.user:
+        	return
 
-    if message.content == "meow":
-        await message.channel.send("Meow")
+	messages = message.content.lower()
+
+    	if messages == "meow":
+        	await message.channel.send("Meow")
+	
+    	if message.author.id == 496892287473680384:
+		reactions = ["⏸️", "🇴", "💀", "🇼", "🇮", "🇳", "🇩", "🅾️", "🪟"]
+		for reaction in reactions:
+		    await message.add_reaction(reaction)
+		await client.process_commands (message)
     
-    author_messages = {
+    	author_messages = {
         496892287473680384: ["Prince Smells","Prince does not own Window"], 
         510637211612479489: ["Steven Hem Chheng does not own a house", "Mongolian"], 
         337091661228408835: ["hahrahr your mother","White Cat should be CEO"],
 	240974979263496192:["harharhar"],
-    }
+    	}
 
-    if message.author.id == 496892287473680384:
-        reactions = ["⏸️", "🇴", "💀", "🇼", "🇮", "🇳", "🇩", "🅾️", "🪟"]
-        for reaction in reactions:
-            await message.add_reaction(reaction)
-        await client.process_commands (message)
     
-    if message.author.id in author_messages:
-        messages = author_messages[message.author.id]
-        selected_message = random.choice(messages)
-        await message.channel.send(selected_message)
+	if message.author.id in author_messages:
+		messages = author_messages[message.author.id]
+		selected_message = random.choice(messages)
+		await message.channel.send(selected_message)
 
-    await client.process_commands(message)
+	await client.process_commands(message)
 
 
 client.run("")
