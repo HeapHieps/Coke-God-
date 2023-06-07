75a106665b51b4b6123a99ef5a9f830fa3a67d59
HeapHieps <117422956+HeapHieps@users.noreply.github.com>
2023-06-06 21:07:21 -0700

    Update CokeGod.py

--- CokeGod.py
+++ CokeGod.py
@@ -7,12 +7,12 @@ client = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
 @client.event
 async def on_ready():
-    	print("Logged in as {0.user}".format(client))
+    print("Logged in as {0.user}".format(client))
 
 
 @client.command()
 async def ping(ctx, arg):
-    	await ctx.send(arg)
+    await ctx.send(arg)
 
 @client.command()
 async def meoow(ctx, member: discord.Member):
@@ -25,41 +25,42 @@ async def meoow(ctx, member: discord.Member):
 
 @client.event
 async def on_message(message):
-	messages = message.content.lower() #Will make all receiving messages to be in lowercase 
-	
-	if message.author == client.user:
-        	return
-
-    	if messages == "meow":
-        	await message.channel.send("Meow")
-	
-    	if message.author.id == 496892287473680384:
-		reactions = ["⏸️", "🇴", "💀", "🇼", "🇮", "🇳", "🇩", "🅾️", "🪟"]
-		for reaction in reactions:
-		    await message.add_reaction(reaction)
-		await client.process_commands (message)
+    if message.author == client.user:
+        return
     
-    	author_messages = {
-        496892287473680384: ["Prince Smells","Prince does not own Window"], 
-        510637211612479489: ["Steven Hem Chheng does not own a house", "Mongolian"], 
-        337091661228408835: ["hahrahr your mother","White Cat should be CEO"],
-	240974979263496192:["harharhar"],
-    	}
+    messages = message.content.lower() #Will check all messages in lowercase
 
-    	check_messages = {
-        	"ceo":"White Cat CEO",
-        	"har":"Harharharh"
-    	}
+    if messages == "meow":
+        await message.channel.send("Meow")
+
+    if message.author.id == 496892287473680384:
+        reactions = ["⏸️", "🇴", "💀", "🇼", "🇮", "🇳", "🇩", "🅾️", "🪟"]
+        for reaction in reactions:
+            await message.add_reaction(reaction)
+        await client.process_commands (message)
     
-	if message.author.id in author_messages:
-		messages = author_messages[message.author.id]
-		selected_message = random.choice(messages)
-		await message.channel.send(selected_message)
-	
-	if messages in check)messages:
-		await message.channel.send(check_messages[messages])
-	
-	await client.process_commands(message)
+    author_messages = {
+        496892287473680384: ["Prince Smells","Prince does not own Window"], #Prince
+        510637211612479489: ["Steven Hem Chheng does not own a house", "Mongolian"], #Steven
+        337091661228408835: ["hahrahr your mother","White Cat should be CEO"], # Cente
+    }
+
+    check_messages = {
+        "ceo":"White Cat CEO",
+        "har":"Harharharh"
+    }
+
+    if message.author.id in author_messages:
+        messages = author_messages[message.author.id]
+        selected_message = random.choice(messages)
+        await message.channel.send(selected_message)
+
+    for word in messages.split():
+         if word in check_messages:
+            await message.channel.send(check_messages[word])
+            break
+
+    await client.process_commands(message)
 
 
-client.run("")
+client.run("MTA2NTE0ODY4ODgwNjE5MTI0NA.GvJQoE.KYUhyPEqM003LpVySHcaYdQITkIjDClWgZD9vs")
