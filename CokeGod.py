fa3840703ed37444e096c4fab861b96d0702d121
HeapHieps <117422956+HeapHieps@users.noreply.github.com>
2023-06-07 20:14:31 -0700

    Update CokeGod.py

--- CokeGod.py
+++ CokeGod.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import random
 
 client = commands.Bot(command_prefix="!", intents=discord.Intents.all())
-
+client.dm_messages = True
 
 @client.event
 async def on_ready():
@@ -28,16 +28,15 @@ async def on_message(message):
     if message.author == client.user:
         return
     
-    messages = message.content.lower() #Will check all messages in lowercase
+    Remessages = message.content.lower() #Will check all messages in lowercase
 
-    if messages == "meow":
+    if Remessages == "meow":
         await message.channel.send("Meow")
 
     if message.author.id == 496892287473680384:
         reactions = ["⏸️", "🇴", "💀", "🇼", "🇮", "🇳", "🇩", "🅾️", "🪟"]
         for reaction in reactions:
             await message.add_reaction(reaction)
-        await client.process_commands (message)
     
     author_messages = {
         496892287473680384: ["Prince Smells","Prince does not own Window"], 
